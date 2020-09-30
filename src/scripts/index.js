128412674c84b52cf4b8552edcceec02cbf3634f
exs1st <prostodan4ikn@ya.ru>
2020-09-30 11:32:31 +0300

    Initial commit with webpack config

---
+++ src/scripts/index.js
@@ -0,0 +1,11 @@
+function SetWindowTitle() {
+	let lang = 'RU';
+
+	if (lang === 'RU') {
+		document.title = 'Даниил Парфенов';
+	} else {
+		document.title = 'Daniil Parfenov';
+	}
+}
+
+SetWindowTitle();
